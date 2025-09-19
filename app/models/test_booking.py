c6cfecab945f85722f0500346a74bbc27fbd4f8d
Qamber ali <qamber246@gmail.com>
2025-09-19 14:54:11 +0500

    modal updated!

--- app/models/test_booking.py
+++ app/models/test_booking.py
@@ -10,12 +10,11 @@ class TestBooking(db.Model):
     gender = db.Column(db.Enum("Male", "Female", "Other", name="gender_enum"), nullable=False)
     age = db.Column(db.Integer, nullable=True)   # only years
     contact_no = db.Column(db.String(15), nullable=False)
-
+    technician_comments = db.Column(db.JSON, nullable=True)
     referred_dr = db.Column(db.Integer, nullable=True)
     referred_non_dr = db.Column(db.Integer, nullable=True)
     give_share_to = db.Column(db.Integer, default=True)
     branch_id = db.Column(db.Integer, nullable=False)
-
     total_no_of_films = db.Column(db.Integer, default=0)
     total_no_of_films_used = db.Column(db.Integer, default=0)
     reason_for_more_films = db.Column(db.String(225), nullable=True)
